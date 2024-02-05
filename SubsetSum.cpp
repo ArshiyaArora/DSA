92f5627c813bd1c6cba4ff08473f3961a828b108
Dusk <141601542+ArshiyaArora@users.noreply.github.com>
2024-02-05 16:36:52 +0530

    Update SubsetSum.cpp

--- SubsetSum.cpp
+++ SubsetSum.cpp
@@ -58,3 +58,23 @@ int main()
 } 
 
 // } Driver Code Ends
+
+//optimised code
+bool subsetSumToK(int n, int k, vector<int> &arr) {
+    vector<bool> prev(k+1,0), curr(k+1,0);
+    prev[0]=curr[0]=true;
+    prev[arr[0]]= true;
+
+    for(int i=1;i<n;i++){
+        for(int target=1;target<=k;target++){
+            bool take=false;
+            if(target>=arr[i]){
+                take=prev[target-arr[i]];
+            }
+            bool notTake=prev[target];
+            curr[target]=take | notTake;
+        }
+        prev=curr;
+    }
+    return prev[k];
+}
