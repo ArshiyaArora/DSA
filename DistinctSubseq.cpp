2c2b4ba60bcd5be7599fa5518ee78b6d9702530b
Dusk <141601542+ArshiyaArora@users.noreply.github.com>
2024-03-09 01:31:07 +0530

    Create DistinctSubseq.cpp

---
+++ DistinctSubseq.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+        vector<long long> prev(m + 1, 0);
+        prev[0]=1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = m; j>=1; j--) {
+                long long take = 0;
+                if (s[i - 1] == t[j - 1]) {
+                    take = prev[j - 1];
+                }
+                int notTake = prev[j];
+                prev[j] = take + notTake;
+            }
+        }
+        return static_cast<int>(prev[m]);
+    }
+};
