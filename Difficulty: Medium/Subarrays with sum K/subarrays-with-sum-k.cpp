da708d5de613bfd934cecacc770cb6d923b72d52
Dusk <141601542+ArshiyaArora@users.noreply.github.com>
2024-06-22 16:49:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int findSubArraySum(int Arr[], int N, int k)
+    {
+        unordered_map<int,int> m;
+        int currSum=0;
+        int freq=0;
+        m[0]=1;
+        for(int i=0;i<N;i++){
+            currSum+=Arr[i];
+            
+            int diff =  currSum - k;
+            freq+= m[diff];
+            m[currSum]++;
+        }
+        return freq;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        int Arr[N];
+        for(int i=0;i<N;i++)    
+            cin>>Arr[i];
+        int k;
+        cin>>k;
+        Solution obj;
+        cout<<obj.findSubArraySum(Arr, N, k)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
