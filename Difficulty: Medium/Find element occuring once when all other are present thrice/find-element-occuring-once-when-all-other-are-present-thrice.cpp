dc25022c0ef765dd00918b4a501ede96274acb4b
Dusk <141601542+ArshiyaArora@users.noreply.github.com>
2024-06-23 15:48:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find element occuring once when all other are present thrice/find-element-occuring-once-when-all-other-are-present-thrice.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int singleElement(int arr[] ,int N) {
+        sort(arr,arr+N);
+        for(int i=0;i<N;){
+            if(arr[i]==arr[i+1]){
+                i+= 3;
+            }
+            else return arr[i];
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        int arr[N];
+        
+        for(int i=0 ; i<N ; i++)
+            cin>>arr[i];
+
+        Solution ob;
+        cout<<ob.singleElement(arr,N);
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
